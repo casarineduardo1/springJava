e7788fef90ce1357212236ceecadc620076d6189
Eduardo Casarin <eduardo.casarin@hotmail.com>
2020-02-19 19:40:33 -0300

    ajustes git

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into cidade (nome, estado)values('Blumenau', 'SC');
-insert into cidade (nome, estado)values('Teste', 'RSs');
-
-
-insert into cliente (nome_completo, sexo, dt_nascimento, idade,cidade_id) values('Teste Jpa', 'M', '2020-05-31', 20, 1);
-insert into cliente (nome_completo, sexo, dt_nascimento, idade,cidade_id) values('Eduardo Casarin', 'M', '2020-05-31', 20, 1);
